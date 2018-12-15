af1e6605cd529393e8587d3f1158163e965f52e4
Raphael Roberts <raphael.roberts48@gmail.com>
2018-12-15 15:07:23 -0600

    started packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+#Argomirr, original author but project is dead; see fork
+setup(name='py7z',
+      version='1.0',
+      description='Python wrapper around 7-zip',
+      url='https://github.com/rlbr/py7z',
+      author='Raphael Roberts',
+      author_email='raphael.roberts48@gmail.com',
+      license='GPLv2',
+      packages=['py7z'],
+      zip_safe=False)
